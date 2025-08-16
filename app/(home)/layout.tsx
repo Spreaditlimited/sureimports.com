db3f5e0904cf68dd438002447cdb05a4520f931f
Emmanuel Atsu <zioncodex@gmail.com>
2025-08-16 13:15:19 +0100

    Sure Imports Landing Page Update7

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,6 +2,7 @@
 
 import Footer from '@/components/home/Footer';
 import NavBar from '@/components/home/NavBar';
+import WhatsAppButton from '@/components/WhatsAppButton';
 import { ReactNode } from 'react';
 import { Suspense } from 'react';
 
@@ -16,7 +17,14 @@ const HomeLayout = ({ children }: HomeLayoutProps) => {
           <title>Sure Imports - Home</title>
         </head>
         <body className="bg-white text-gray-900 antialiased">
-          <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">
+            {children}
+          <WhatsAppButton
+            waID="CUR7YKW3K3RBA1"
+            message="Hello! I'd like to ask about your services."
+            position="bottom-left"
+          />
+          </main>
         </body>
       </html>
   );
