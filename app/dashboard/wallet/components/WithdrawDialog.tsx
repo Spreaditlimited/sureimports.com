c95790a7fa7f5b2e329dba737054d6e17c3a7688
Emmanuel Atsu <zioncodex@gmail.com>
2025-09-21 09:40:31 +0100

    Paystack Refund7

--- app/dashboard/wallet/components/WithdrawDialog.tsx
+++ app/dashboard/wallet/components/WithdrawDialog.tsx
@@ -122,7 +122,7 @@ export default function WithdrawDialog({ isOpen, onClose, walletBalance, onWithd
     toast.success('Processing withdrawal request...');
 
     // Call the refund API with the proper amount and currency
-    initiateRefund('258116764', withdrawalAmount)
+    initiateRefund('000015250905062953000000242556', withdrawalAmount)
       .then((data) => {
         console.log('Withdrawal successful:', data);
         
