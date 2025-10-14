b95d1c290e6203d95fe73076fa7d9e09e509ce53
Emmanuel Atsu <zioncodex@gmail.com>
2025-10-14 07:38:15 +0100

    Manual Cron Job Update

--- app/api/cron/delete-orders/route.ts
+++ app/api/cron/delete-orders/route.ts
@@ -157,7 +157,7 @@ The Sure Imports Team
 
         //EMAIL 3: Send email at 2 days
         //(daysPending == 7)
-        if(xEmail==='email'){
+        if('email'==='email'){
             const xTitle = `Final Reminder: Your Order Will Be Cancelled Tomorrow (Order ID: ${order.pidOrder})`;
             const personalizedBody = xBody3
                   .replace('[First Name]', user.userFirstname as any)
