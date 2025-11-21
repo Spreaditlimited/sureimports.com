a3de329029888eac9808c0802f76118c860f01d2
Emmanuel Atsu <zioncodex@gmail.com>
2025-11-21 09:13:39 +0100

    Shop Feature 4

--- components/dashboard/sidenavbar/constants/side-nav.tsx
+++ components/dashboard/sidenavbar/constants/side-nav.tsx
@@ -3,39 +3,34 @@
 import {
   Download,
   HandCoins,
-  Instagram,
   Package,
   Settings,
   ShoppingBag,
-  ShoppingBasketIcon,
   ShoppingCart,
-  ShoppingCartIcon,
   User2,
   Wallet,
-} from 'lucide-react';
-import {
-  Category,
-  Frame,
-  Pay,
-  SpecialSource,
-  Shipping,
-  BuyPhones,
-  Shop,
-  Message,
-  Tiktok,
-  Logout,
+  Globe,
+  Search,
+  Smartphone,
+  DollarSign,
+  Truck,
+  CheckCircle,
+  Package2,
+  BookOpen,
+  UserPlus,
+  MessageSquare,
+  LogOut,
+  Video,
   Youtube,
-  Academy,
-  Affiliate,
-} from '../components/icons/category';
+  Instagram,
+} from 'lucide-react';
 import { type NavItem } from '../types';
 import { BsInstagram, BsTiktok, BsYoutube } from 'react-icons/bs';
-import { BiMoney } from 'react-icons/bi';
 
 export const NavItems: NavItem[] = [
   {
     title: 'Videos',
-    icon: Category,
+    icon: Video,
     href: '/',
     target: '_blank',
     color: 'text-white',
@@ -54,18 +49,6 @@ export const NavItems: NavItem[] = [
         color: 'text-white',
         href: 'https://youtube.com/@sureimports?si=Ff0Ziwec36sACB2g',
       },
-      // {
-      //   title: 'Youtube',
-      //   icon: BsYoutube,
-      //   color: 'text-white',
-      //   href: 'https://www.youtube.com/embed/MR9Sxx4dRaM',
-      // },
-      // {
-      //   title: 'Instagram',
-      //   icon: BsInstagram,
-      //   color: 'text-white',
-      //   href: 'https://www.instagram.com/sureimport',
-      // },
     ],
   },
   {
@@ -76,49 +59,31 @@ export const NavItems: NavItem[] = [
   },
   {
     title: 'Buy From Chinese Websites',
-    icon: Shop,
+    icon: Globe,
     href: '/dashboard/procurement',
     color: 'text-white',
   },
   {
     title: 'Buy Phones & Laptops',
-    icon: ShoppingCartIcon,
+    icon: Smartphone,
     href: '/dashboard/store?id=laptop',
     color: 'text-white',
   },
   {
     title: 'Special Sourcing',
-    icon: SpecialSource,
+    icon: Search,
     href: '/dashboard/special-sourcing/pending',
     color: 'text-white',
   },
-  // {
-  //   title: 'Pay Supplier',
-  //   icon: Pay,
-  //   href: '/dashboard/pay-supplier/saved',
-  //   color: 'text-white',
-  // },
-  // {
-  //   title: 'Shipping only',
-  //   icon: Shipping,
-  //   href: '/dashboard/shipping-only/request-received',
-  //   color: 'text-white',
-  // },
   {
     title: 'Verify Supplier',
-    icon: Pay,
+    icon: CheckCircle,
     href: '/dashboard/verify-supplier/pending-payment',
     color: 'text-white',
   },
-  // {
-  //   title: 'Buy Phones',
-  //   icon: BuyPhones,
-  //   href: '/dashboard/buy-phones',
-  //   color: 'text-white',
-  // },
   {
     title: 'Refunds',
-    icon: BiMoney,
+    icon: DollarSign,
     href: '/dashboard/refunds',
     color: 'text-white',
   },
@@ -134,21 +99,6 @@ export const NavItems: NavItem[] = [
     href: '/dashboard/orders',
     color: 'text-white',
   },
-  // {
-  //   title: 'Buy Phones',
-  //   icon: BuyPhones,
-  //   href: '/',
-  //   color: 'text-white',
-  //   isChidren: true,
-  //   children: [
-  //     {
-  //       title: 'Download',
-  //       icon: Download,
-  //       color: 'text-white',
-  //       href: '/',
-  //     },
-  //   ],
-  // },
   {
     title: 'Pay Small Small',
     icon: HandCoins,
@@ -157,7 +107,7 @@ export const NavItems: NavItem[] = [
   },
   {
     title: 'Resources',
-    icon: ShoppingCartIcon,
+    icon: BookOpen,
     href: 'https://spreadit.selar.co',
     color: 'text-white',
   },
@@ -167,36 +117,6 @@ export const NavItems: NavItem[] = [
     href: 'https://calendly.com/sureimports/product-sourcing-one-one-one-session/',
     color: 'text-white',
   },
-  // {
-  //   title: 'Blog',
-  //   icon: Academy,
-  //   href: `${process.env.NEXT_PUBLIC_ACADEMY_SITE_URL}`,
-  //   color: 'text-white',
-  // },
-  // {
-  //   title: 'Become an Affiliate',
-  //   icon: Affiliate,
-  //   href: `${process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL}`,
-  //   color: 'text-white',
-  // },
-  // {
-  //   title: 'Inbox',
-  //   icon: Message,
-  //   href: '/dashboard/message/message-box',
-  //   color: 'text-white',
-  // },
-  // {
-  //   title: 'Settings',
-  //   icon: Settings,
-  //   href: '/dashboard/profile-update',
-  //   color: 'text-white',
-  // },
-  // {
-  //   title: 'Logout',
-  //   icon: Logout,
-  //   href: '/auth/logout',
-  //   color: 'text-white',
-  // },
 ];
 
 //MOBILE MENU
@@ -204,7 +124,7 @@ export const NavItems: NavItem[] = [
 export const MobileNavItems: NavItem[] = [
   {
     title: 'Videos',
-    icon: Category,
+    icon: Video,
     href: '/',
     color: 'text-white',
     isChidren: true,
@@ -222,15 +142,9 @@ export const MobileNavItems: NavItem[] = [
         color: 'text-white',
         href: 'https://youtube.com/@sureimports?si=Ff0Ziwec36sACB2g',
       },
-      // {
-      //   title: 'Instagram',
-      //   icon: BsInstagram,
-      //   color: 'text-white',
-      //   href: 'https://www.instagram.com/sureimport',
-      // },
     ],
   },
-    {
+  {
     title: 'Shop',
     icon: ShoppingBag,
     href: '/dashboard/shop',
@@ -238,49 +152,31 @@ export const MobileNavItems: NavItem[] = [
   },
   {
     title: 'Buy From Chinese Websites',
-    icon: Frame,
+    icon: Globe,
     href: '/dashboard/procurement',
     color: 'text-white',
   },
   {
     title: 'Buy Phones & Laptops',
-    icon: ShoppingCartIcon,
+    icon: Smartphone,
     href: '/dashboard/store?id=laptop',
     color: 'text-white',
   },
   {
     title: 'Special Sourcing',
-    icon: SpecialSource,
+    icon: Search,
     href: '/dashboard/special-sourcing/pending',
     color: 'text-white',
   },
-  // {
-  //   title: 'Pay Supplier',
-  //   icon: Pay,
-  //   href: '/dashboard/pay-supplier/saved',
-  //   color: 'text-white',
-  // },
-  // {
-  //   title: 'Shipping only',
-  //   icon: Shipping,
-  //   href: '/dashboard/shipping-only/request-received',
-  //   color: 'text-white',
-  // },
   {
     title: 'Verify Supplier',
-    icon: Pay,
+    icon: CheckCircle,
     href: '/dashboard/verify-supplier/pending-payment',
     color: 'text-white',
   },
-  // {
-  //   title: 'Buy Phones',
-  //   icon: BuyPhones,
-  //   href: '/dashboard/buy-phones',
-  //   color: 'text-white',
-  // },
   {
     title: 'Refunds',
-    icon: BiMoney,
+    icon: DollarSign,
     href: '/dashboard/refunds',
     color: 'text-white',
   },
@@ -302,25 +198,9 @@ export const MobileNavItems: NavItem[] = [
     href: '/dashboard/pay-small-small?status=SAVED',
     color: 'text-white',
   },
-  // {
-  //   title: 'Buy Phones',
-  //   icon: BuyPhones,
-  //   href: '/',
-  //   color: 'text-white',
-  //   isChidren: true,
-  //   children: [
-  //     {
-  //       title: 'Download',
-  //       icon: Download,
-  //       color: 'text-white',
-  //       href: '/',
-  //     },
-  //   ],
-  // },
-
-    {
+  {
     title: 'Resources',
-    icon: ShoppingCartIcon,
+    icon: BookOpen,
     href: 'https://spreadit.selar.co',
     color: 'text-white',
   },
@@ -330,23 +210,4 @@ export const MobileNavItems: NavItem[] = [
     href: 'https://calendly.com/sureimports/product-sourcing-one-one-one-session/',
     color: 'text-white',
   },
-  // {
-  //   title: 'Blog',
-  //   icon: Academy,
-  //   href: `${process.env.NEXT_PUBLIC_ACADEMY_SITE_URL}`,
-  //   color: 'text-white',
-  // },
-
-  // {
-  //   title: 'Become an Affiliate',
-  //   icon: Affiliate,
-  //   href: `${process.env.NEXT_PUBLIC_AFFILIATE_SITE_URL}`,
-  //   color: 'text-white',
-  // },
-  // {
-  //   title: 'Logout',
-  //   icon: Logout,
-  //   href: '/auth/logout',
-  //   color: 'text-white',
-  // },
 ];
